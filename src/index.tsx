177fa9e7678abf72788a3631806921d7bb27334b
adfharrsion <68917507+adfharrsion@users.noreply.github.com>
2021-03-24 13:16:37 +0000

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,50 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect, useRef } from 'react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App: React.FC = () => {
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // function to initialise esBuild, using the esbuild.wasm in public dir. assign it to ref
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+  // start service jsut once, on initial render
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    // makes sure esBuild service has started
+    if (!ref.current) {
+      return;
+    }
+    // use transform method of esBuild stored in ref, feed in textarea content, and some options
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+    setCode(result.code);
+  };
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
